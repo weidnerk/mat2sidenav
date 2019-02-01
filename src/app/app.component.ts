3d859fa98b729e13d491ec4cbc4968d5ba1e82cf
Kevin Weidner <weidnerk@gmail.com>
2019-02-01 16:03:20 -0500

    initial load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mat2sidenav';
+
+  routes = [
+    { path: '/', name: 'Home' },
+    { path: 'one', name: 'One' },
+    { path: 'two', name: 'Two' },
+    { path: 'three', name: 'Three' }
+  ];
+
+  constructor() {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
+import {MatButtonModule, MatCardModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {OneComponent} from './one/one.component';
+import {TwoComponent} from './two/two.component';
+import {ThreeComponent} from './three/three.component';
+import {ThreeModule} from './three/three.module';
+import {OneModule} from './one/one.module';
+import {TwoModule} from './two/two.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OneComponent,
+    TwoComponent,
+    ThreeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    MatSidenavModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+
+    AppRoutingModule,
+    OneModule,
+    TwoModule,
+    ThreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
